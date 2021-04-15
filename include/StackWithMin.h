965361c7c7ff9005cbc077a5fc059ae1894ffa35
oimlock <dmitriy.goncharov.1994@gmail.com>
2021-04-15 22:49:43 +0200

    Refactor project to build with CMake

---
+++ include/StackWithMin.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace oimlock {
+
+struct Node {
+    int value;
+    int current_min;
+    Node* next;
+
+    Node(int value, int current_min);
+};
+
+class Stack {
+public:
+    Stack();
+    ~Stack();
+    bool empty() const;
+    void push(int x);
+    void pop();
+    int top() const;
+    int min() const;
+
+private:
+    Node* begin_;
+};
+
+}  // namespace oimlock
